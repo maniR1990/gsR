03227def79025be5e368a3c22334c466b41b5508
mani <mani.r16@gmail.com>
2018-11-18 17:00:18 +0530

    apply middle ware in redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes'; // where we are going to specify our routes
 import App from './components/app/app';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import homeReducer from './reducers/homeReducer';
 import "style.scss";
 
-const store = createStore(homeReducer);
+const logAction = store => {
+  return next => {
+    return action => {
+      const result = next(action);
+      console.log(
+        `caught in the middleware ${JSON.stringify(store.getState())}`
+      );
+      return result;
+    };
+  };
+};
+const store = createStore(homeReducer, applyMiddleware(logAction));
 
 ReactDOM.render(
   <Provider store={store}>
